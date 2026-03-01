2b905cd589804faadb9100d7e332cefc9bfd735a
MaiqiVerse <65766132+MaiqiVerse@users.noreply.github.com>
2026-03-01 17:35:41 +0000

    Deploying to gh-pages from @ MaiqiVerse/MaiqiVerse.github.io@eab150221deb7b3f405d00a110a2e66bb4507906 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,7 +94,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%6A%69%61%6E%67%30%34@%77%6D.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -118,11 +118,11 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=ozEndNQAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/maiqi-jiang-103346273", "_blank");
         },
       },{
       id: 'light-theme',
